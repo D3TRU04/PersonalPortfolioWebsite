802b6ba975d846644c507318a268b4ed2bf57742
Dan Truong <68484345+D3TRU04@users.noreply.github.com>
2025-01-05 23:10:53 -0600

    adjusted home page

--- personal_portfolio_website/pages/HomePage.tsx
+++ personal_portfolio_website/pages/HomePage.tsx
@@ -6,9 +6,9 @@ function LandingPage() {
   return (
     <>
       <NavBar activePath="" />
-      <div className="md:grid md:grid-cols-11 md:col-gap-16 lg:col-gap-8 mb-16 md:mb-16">
-        <div className="col-span-5">
-          <h3 className="mb-1 text-4xl lg:text-6xl leading-tight">a brief introduction</h3>
+      <div className="w-full h-screen bg-white dark:bg-black text-black dark:text-white flex md:grid place-items-center items-center justify-center">
+        <div className="text-center">
+          <h3 className="mt-8 mb-1 text-4xl lg:text-6xl leading-tight">a brief introduction</h3>
           <h3 className="text-lg leading-tight pb-2">computer science @ ut austin</h3>
           <h3 className="text-lg leading-tight pb-2">austin • houston</h3>
         </div>
